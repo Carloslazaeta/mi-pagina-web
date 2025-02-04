bde78e177f62058329d5519f17a12135610e9853
Carlos Rojas <Carloss_7@hotmail.com>
2025-02-04 18:49:40 -0500

    Updated all files

--- firebase.js
+++ firebase.js
@@ -1,16 +1,16 @@
-// Import Firebase modules
+// Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxEhmpYBS0-fFxXNYDm6KMFlSoDBqgyF8",
-  authDomain: "employeeprofiles-75652.firebaseapp.com",
-  databaseURL: "https://employeeprofiles-75652-default-rtdb.firebaseio.com",
-  projectId: "employeeprofiles-75652",
-  storageBucket: "employeeprofiles-75652.firebasestorage.app",
-  messagingSenderId: "436210594516",
-  appId: "1:436210594516:web:4d19dcc66d42929d86c141"
+    apiKey: "AIzaSyDxEhmpYBS0-fFxXNYDm6KMFlSoDBqgyF8",
+    authDomain: "employeeprofiles-75652.firebaseapp.com",
+    databaseURL: "https://employeeprofiles-75652-default-rtdb.firebaseio.com",
+    projectId: "employeeprofiles-75652",
+    storageBucket: "employeeprofiles-75652.firebasestorage.app",
+    messagingSenderId: "436210594516",
+    appId: "1:436210594516:web:4d19dcc66d42929d86c141"
 };
 
 // Initialize Firebase
