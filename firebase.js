21573839266570a849fed37de154c93f32f54f31
Carlos Rojas <Carloss_7@hotmail.com>
2025-02-04 20:05:57 -0500

    Updated all files

--- firebase.js
+++ firebase.js
@@ -1,20 +1,20 @@
-// Import Firebase SDK
+// Importar Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
-// Firebase configuration
+// Configuración de Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDxEhmpYBS0-fFxXNYDm6KMFlSoDBqgyF8",
-    authDomain: "employeeprofiles-75652.firebaseapp.com",
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_AUTH_DOMAIN",
     databaseURL: "https://employeeprofiles-75652-default-rtdb.firebaseio.com",
     projectId: "employeeprofiles-75652",
-    storageBucket: "employeeprofiles-75652.firebasestorage.app",
+    storageBucket: "employeeprofiles-75652.appspot.com",
     messagingSenderId: "436210594516",
     appId: "1:436210594516:web:4d19dcc66d42929d86c141"
 };
 
-// Initialize Firebase
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { database, ref, set, get, child };
+export { database, ref, get, set };
